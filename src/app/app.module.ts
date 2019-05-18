565c1d8381737666616778eb0030883565e822a0
Delipa <jorgrandres1461@gmail.com>
2019-05-18 00:09:39 -0500

    Header Y footer

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './login/login.component';
+import { SharedModule } from './shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PagesComponent,
+    LoginComponent,
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
