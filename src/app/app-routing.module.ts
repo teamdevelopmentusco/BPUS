a7cd1bc6db54afe14334dfcd2f6fca59a1e927a2
jesgra <jesus.egranada@gmail.com>
2019-05-18 12:00:14 -0500

    Arreglo de header y footer, creacion componente login y configuracion de rutas (/,/login)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
+=======
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PagesComponent } from './pages/pages.component'
+
+const routes: Routes = [
+  {path: '', component: PagesComponent},
+  {path: 'login', component: LoginComponent}
+  ];
+>>>>>>> Arreglo de header y footer, creacion componente login y configuracion de rutas (/,/login)
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
