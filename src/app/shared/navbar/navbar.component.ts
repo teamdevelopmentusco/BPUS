4f0cf6feea47d95d532920b950cbd7a3e79711f9
Daniel Velasquez <gorsitho@gmail.com>
2019-07-24 05:46:21 -0500

    Cambios en el navbar, dinamico

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SidebarService, UsuarioService } from 'src/app/services/service.index';
+import { Usuario } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  
+  usuario:Usuario;
+  constructor(public _sidebar:SidebarService,public _usuarioService:UsuarioService) { }
 
   ngOnInit() {
+    this.usuario= this._usuarioService.usuario;
+    this._sidebar.cargarMenu();
   }
 
 }
