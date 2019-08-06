6b99c676502cf3e7c9f46dd2d920a92d4d11237d
Julián Andrade <julian19896@gmail.com>
2019-08-06 13:37:29 -0500

    modelos y servicios del anteproyecto, proyecto y articulo

---
+++ src/app/models/articulo.ts
@@ -0,0 +1,17 @@
+/*
+* Proyecto: BPUS
+* Componente: /models/articulo.ts
+* Desarrollador: Cristian Julián Andrade Murillo
+* Descripción: modelo del artículo con los atributos
+* Última modificación: 06/08/2019
+*/
+
+export class Articulo {
+
+    constructor(
+        public rutaArticulo: string,
+        public estado?: boolean,
+        public _id?: string
+    ) {
+    }
+}
