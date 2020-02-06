f9ad883a4edc3663e18d48459a86bd071323ea8e
Daniel Velasquez <gorsitho@gmail.com>
2020-02-06 07:36:18 -0500

    subir archivos

--- src/app/pages/proyectosUsco/proyecto/proyecto.component.ts
+++ src/app/pages/proyectosUsco/proyecto/proyecto.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {formatDate } from '@angular/common';
 
+import swal from 'sweetalert2';
+import { SubirArchivoService, UsuarioService } from 'src/app/services/service.index';
 @Component({
   selector: 'app-proyecto',
   templateUrl: './proyecto.component.html',
@@ -9,13 +11,89 @@ import {formatDate } from '@angular/common';
 export class ProyectoComponent implements OnInit {
   today = new Date();
   jstoday = '';
-
-  constructor() {
+  usuarioId:string;
+  file:File;
+  
+  constructor( public _subirArchivosService: SubirArchivoService, public _usuarioService: UsuarioService) {
     this.jstoday = formatDate(this.today, 'dd/MM/yyyy', 'en-US', '+0530');
+    this.usuarioId= this._usuarioService.usuario._id;
   }
 
   ngOnInit() {
   }
+
+
+  onFilesAdded(file:File ) {
+
+
+    if (!file) {
+       return;
+     }
+  
+     /*if (file.type.indexOf('pdf')<0) {
+  
+      swal.fire('Solo pdfs','El archivo seleccionado no es un pdf','error');
+       return;
+     }*/
+
+
+     
+    console.log(file);
+
+    /*
+      const reader = new FileReader();
+      reader.onload = (e: ProgressEvent) => {
+        const content = (e.target as FileReader).result;
+        // this content string could be used directly as an image source
+        // or be uploaded to a webserver via HTTP request.
+        console.log(content);
+
+        
+      };  
+      // use this for basic text files like .txt or .csv
+      //reader.readAsText(file);
+      // use this for images
+      reader.readAsText(file);*/
+
+     this.file = file;
+
+  
+   
+  }
+
+
+
+
+  subirArchivo(){
+
+
+    this._subirArchivosService.subirArcivo(this.file,'proyectos',this.usuarioId).then((resp:any)=>{
+
+      swal.fire('Archivo subido con exito.',this.usuarioId,'success');
+
+
+    }).catch(resp =>{
+        console.log(resp);
+    });;
+
+
+
+
+
+  }
+
+
+
+
+
+    
+
+
+
+
+
+  /*
+
 
   onFilesAdded(files: File[]) {
     console.log(files);
@@ -34,6 +112,12 @@ export class ProyectoComponent implements OnInit {
       // reader.readAsDataURL(file);
     });
   }
+
+
+  */
+
+
+
   onFilesRejected(files: File[]) {
     console.log(files);
   }
